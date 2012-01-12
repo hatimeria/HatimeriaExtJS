5d22545b5f8d60bb2cfe56f8b4146ffc1b3f9295
Zbigniew Łabacz <zbigniew.labacz@hatimeria.pl>
2012-01-12 13:59:47 +0100

    resolve conflict.

--- src/core/grid/BaseGrid.js
+++ src/core/grid/BaseGrid.js
@@ -5,10 +5,52 @@
     
     Ext.define('Hatimeria.core.grid.BaseGrid', {
         extend: 'Ext.grid.Panel',
+        alternateClassName: 'HatimeriaAdmin.core.grid.BaseGrid',
+        mixins: {
+            configurable: 'Hatimeria.core.mixins.ConfigurableExternal'
+        },
         
+        /**
+         * Row operations
+         * 
+         * @cfg {Array}/{String}/{Object} rowActions
+         * @example
+         *     rowActions: ['edit', 'remove', 'clone']
+         *  or
+         *     rowActions: {
+         *       edit: 'Edycja',
+         *       remove: 'Usuń',
+         *       clone: 'Klonuj',
+         *     }
+         *  or   
+         *     rowActions: "edit, remove, string" 
+         */
+        rowActions: {},
+        
+        /**
+         * Class name of edit window
+         * 
+         * @property {String}
+         */
+        windowEditClass: false,
+        
+        /**
+         * @property {Array} rowActions
+         * @private
+         */
+        defaultRowActions: {
+            edit: 'Edycja',
+            remove: 'Usuń',
+            clone: 'Klonuj'
+        },
+        
+        /**
+         * Initializes component
+         */
         initComponent: function()
         {
             this.callParent();
+            var actions = this.getRowActions();
             
             this.on({
                 itemcontextmenu: {
@@ -18,45 +60,114 @@
                 itemdblclick: {
                     scope: this, 
                     fn: function(grid, record, el, index) {
-                        this.onEditClick(record, index);
+                        if (actions['edit'])
+                        {
+                            this.onEditClick(record, index);
+                        }
                     }
                 }
             });
         },
         
         /**
+         * Row Actions
+         *
+         * @private
+         * @return {Object}
+         */
+        getRowActions: function()
+        {
+            if (Ext.isArray(this.rowActions) || Ext.isString(this.rowActions))
+            {
+                if (Ext.isString(this.rowActions))
+                {
+                    this.rowActions = this.rowActions.split(/ ?, ?/g);
+                }
+                if (Ext.isArray(this.rowActions))
+                {
+                    var actions = {};
+                    for (var i=0; i<this.rowActions.length; i++)
+                    {
+                        var item = this.rowActions[i];
+                        actions[item] = this.defaultRowActions[item];
+                    }
+                    this.rowActions = actions;
+                }
+            }
+            
+            return this.rowActions;
+        },
+        
+        /**
+         * Edit window class
+         * 
+         * @return {String}
+         */
+        getWindowEditClass: function()
+        {
+            return this.windowEditClass;
+        },
+        
+        /**
+         * Merges external config
+         * 
+         * @param {Object} config
+         * @return {Object}
+         */
+        applyExternal: function(cfg)
+        {
+            var config = this.getConnectedConfig();
+            
+            if (config && config.columns)
+            {
+                for (var i in config.columns)
+                {
+                    cfg.columns.push(config.columns[i]);
+                }
+            }
+            
+            return cfg;
+        },
+        
+        /**
+         * Yes/No renderer
+         * 
+         * @return {String}
+         */
+        rendererYesNo: function(value)
+        { 
+            return value ? 'Tak': 'Nie' 
+        },
+        
+        /**
          * Items for context menu
          * 
-         * @return []
+         * @return {Array}
          */
         getContextMenuItems: function(record, index)
         {
-            return [
-                {
-                    text: 'Edytuj',
-                    cls: 'ux-edit',
+            var items = [];
+            var actions = this.getRowActions();
+            
+            for (var name in actions)
+            {
+                items.push({
+                    text: actions[name],
+                    cls: 'ux-' + name,
                     scope: this,
                     handler: function() {
-                        this.onEditClick(record, index);
+                        this['on' + Ext.capitalize(name) + 'Click'](record, index);
                     }
-                },
-                {
-                    text: 'Usuń',
-                    cls: 'ux-remove',
-                    scope: this,
-                    handler: function() {
-                        this.onRemoveClick(record, index);
-                    }
-                }
-            ];
+                });
+            }
         },
         
         /**
          * Event: right click on row
          * 
-         * @param Ext.tree.Panel
-         * @param Ext.data.Node
-         * @param DOMElement el
+         * @param {Ext.tree.Panel} grid
+         * @param {Ext.data.Node} record
+         * @param {DOMElement} el
          * @param int index
          * @param Ext.Object.Event event
          */
@@ -75,22 +186,47 @@
          * @param Ext.data.Model record
          * @param int index
          */
-        onEditClick: function(record, index)
+        onEditClick: function(record)
         {
-            // Abstract
+            var editWindow = Ext.create(this.getWindowEditClass());
+            editWindow.show();
+            editWindow.populate(record);
         },
-
+        
         /**
-         * Event: edit click
+         * Cloning record
          * 
          * @param Ext.data.Model record
          * @param int index
          */
-        onRemoveClick: function(record, index)
+        onCloneClick: function(record, index)
         {
-            // Abstract
+            var editWindow = Ext.create(this.getWindowEditClass());
+            editWindow.show();
+            var newRecord = record.copy();
+            newRecord.setId('');
+            editWindow.populate(newRecord);
+        },
+
+        /**
+         * Event: remove click
+         * 
+         * @param Ext.data.Model record
+         */
+        onRemoveClick: function(record)
+        {
+            var store = this.store;
+            Ext.Msg.confirm('Uwaga', 'Nastąpi usunięcie rekordu z bazy danych.<br/>Czy kontynuować?', function(response) {
+                if (response == 'yes')
+                {
+                    record.destroy({
+                        success: function() {
+                            store.load();
+                        }
+                    });
+                }
+            });
         }
-        
     });
     
 })();
